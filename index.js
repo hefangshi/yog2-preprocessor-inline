3b8d8eb363a9516e03d7580bdddc37d306bdef78
hefangshi <hefangshi@gmail.com>
2015-07-01 17:01:17 +0800

    [upd] for yog2

--- index.js
+++ index.js
@@ -21,20 +21,23 @@ function doInline(content, file, ext, type) {
         return content;
     }
     if (type === 'css') {
-        content = ld + 'style id="' + inline +'"' + rd + fis.compile.extCss('@import url("' + inline + '?__inline")') + ld + '/style' + rd + content;
-    } else if (type === 'js') {
-        content = ld + 'script id="' + inline +'"' + rd + fis.compile.extJs('__inline("' + inline + '")') + ld + '/script' + rd + content;
+        content = ld + 'style id="' + inline + '"' + rd + fis.compile.extCss('@import url("' + inline + '?__inline")') +
+            ld + 'endstyle' + rd + content;
+    }
+    else if (type === 'js') {
+        content = ld + 'script id="' + inline + '"' + rd + fis.compile.extJs('__inline("' + inline + '")') + ld +
+            'endscript' + rd + content;
     }
     return content;
 }
 
-module.exports = function(content, file, conf) {
+module.exports = function (content, file, conf) {
     if (file.rExt !== '.tpl') {
         return content;
     }
 
-    ld = conf.left_delimiter || fis.config.get('settings.smarty.left_delimiter') || '{%';
-    rd = conf.right_delimiter || fis.config.get('settings.smarty.right_delimiter') || '%}';
+    ld = conf.left_delimiter || fis.config.get('settings.swig.left_delimiter') || '{%';
+    rd = conf.right_delimiter || fis.config.get('settings.swig.right_delimiter') || '%}';
     var exts = fis.config.get('roadmap.ext') || {};
 
     exts = fis.util.merge(exts, {
@@ -45,6 +48,6 @@ module.exports = function(content, file, conf) {
     fis.util.map(exts, function (ext, type) {
         content = doInline(content, file, ext, type);
     });
-    
+
     return content;
 };
